2b5d6770088675f970d983b3ad33442f25f1c76d
MarcusKM95 <mamo0005@stud.kea.dk>
2023-11-13 13:01:01 +0100

    method used commit

--- src/main/java/Dog.java
+++ src/main/java/Dog.java
@@ -1,7 +1,12 @@
 public class Dog extends Animal{
 
+    public Dog(String name){
+        super(name);
+    }
+
     @Override
     public void makeSound() {
+        System.out.println("Vuf");
 
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
-package org.example;
-
 public class Main {
     public static void main(String[] args) {
-        
+        Dog dog1 = new Dog("Hans");
+        dog1.makeSound();
+
     }
 }
