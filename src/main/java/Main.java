4115904842599fb0a2a315dc5e77ac93b9eb62a1
MarcusKM95 <mamo0005@stud.kea.dk>
2023-11-13 13:26:29 +0100

    cat added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,8 @@
 public class Main {
     public static void main(String[] args) {
         Dog dog1 = new Dog("Hans");
+        Cat cat1 = new Cat("Ole");
+        cat1.makeSound();
         dog1.makeSound();
     }
 }
